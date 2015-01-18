43a903626d7fbfa7894d4d562dbf84f0b82e14ef
Rui Mendes <ruidamendes@ua.pt>
2015-01-18 15:48:01 +0000

    Created plot2.R file

---
+++ Project2/plot2.R
@@ -0,0 +1,42 @@
+## Exploratory Data Analysys
+## Rui Mendes (ruidamendes@ua.pt)
+## Project2
+## January 2014
+
+## 2. Have total emissions from PM2.5 decreased in the Baltimore City, Maryland (fips == "24510") from 1999 to 2008?
+## Use the base plotting system to make a plot answering this question.
+
+source('helpers.R')
+
+## Load NEI dataset
+NEI <- loadRDSFile("summarySCC_PM25.rds")
+
+## Check the first elements of the dataset
+## print(head(NEI))
+
+## Filter dataset for Baltimore (fips="24510")
+bmoreEmissions <- NEI[NEI$fips=="24510",]
+# group emissions by year
+bmoreEmissionsByYear <- aggregate(Emissions ~ year, bmoreEmissions, sum)
+
+## Force R not to use exponential notation (e.g. e+10)
+options("scipen"=100, "digits"=4)
+
+## Create PNG file (for the plot)
+png(file="plot2.png", height=400)
+
+## Define Y axis limit
+ylim <- c(0, 1.1*max(bmoreEmissionsByYear$Emissions))
+
+## Create the barplot
+emissionsPlot <- barplot(height=bmoreEmissionsByYear$Emissions, names.arg=bmoreEmissionsByYear$year,
+                         xlab="Years", ylab=expression('Total PM'[2.5]*' emissions'), ylim = ylim, 
+                         main=expression('Total PM'[2.5]*' emissions for each year in Baltimore City'), cex.names=1.5,)
+
+## Add values, line and points above each bar
+text(x = emissionsPlot, y = bmoreEmissionsByYear$Emissions, label = round(bmoreEmissionsByYear$Emissions, 0), pos = 3, cex = 0.8, col = "brown")
+lines(x = emissionsPlot, y = bmoreEmissionsByYear$Emissions, col=4)
+points(x = emissionsPlot, y = bmoreEmissionsByYear$Emissions, col=4, pch=16)
+
+## Close file writing
+dev.off()
